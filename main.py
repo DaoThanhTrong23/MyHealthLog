bdd1fca2b478e05cdd7baf9bb04a1b00dca88fc9
DaoThanhTrong23 <daothanhtrong42005@gmail.com>
2025-04-03 21:56:16 +0700

    gom về file main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from home import HomeScreen  # Import giao diện trang chủ
+#from home import HomeScreen  # Import giao diện trang chủ
 from PIL import Image, ImageTk
 class LoginApp:
     def __init__(self):
@@ -74,6 +74,40 @@ class LoginApp:
     def thoat_chuong_trinh(self):
         """Đóng chương trình"""
         self.root.destroy()
+
+#Trang chủ 
+class HomeScreen:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Trang Chủ")
+        
+        # Kích thước cửa sổ
+        window_width = 900
+        window_height = 600
+        
+        # Lấy kích thước màn hình
+        screen_width = self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
+        
+        # Tính toán vị trí để canh giữa
+        position_x = (screen_width // 2) - (window_width // 2)
+        position_y = (screen_height // 2) - (window_height // 2)
+        
+        # Thiết lập kích thước & vị trí
+        self.root.geometry(f"{window_width}x{window_height}+{position_x}+{position_y}")
+
+        btn_logout = tk.Button(self.root, text="Đăng Xuất", command=self.dangxuat_return_login)
+        btn_logout.pack(pady="15",side="top", anchor="e")
+
+
+        label_welcome = tk.Label(self.root, text="Chào mừng bạn đến với Trang Chủ!", font=("Arial", 14))
+        label_welcome.pack(pady=0)
+        
+        self.root.mainloop()
+    def dangxuat_return_login(self):
+        from main import LoginApp
+        self.root.destroy()
+        LoginApp()
 
 if __name__ == "__main__":
     LoginApp()
--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-
-icon_path = "Images/logo.png"
-
-if os.path.exists(icon_path):
-    print("✅ Ảnh tồn tại:", icon_path)
-else:
-    print("❌ Không tìm thấy ảnh:", icon_path)
